e8214903e98c508754a3b484f1aa620290633cb6
@crazyself <crazyself@gmail.com>
2022-05-27 14:28:58 -0300

    Comit inicial

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# calculadora_matrizes
+# Caculadora de matrizes
+
+## Trabalho realizado na faculdade de matemática - Univesp
+
+Este trabalho consiste em criar uma calculadora de matrizes on-line para auxiliar os alunos de séries diversas no aprendizado de operações com matrizes, tanto em sala de aula como em aulas à distãncia. A calculadora também tem a finalidade de servir como ferramenta de apoio à professores no ensino de álgebra e na inserção da tecnologia como um contraponto ao estudo tradicional lousa-caderno.
+
+Desenvolvido: Almir Martins Lopes
+Iniciado em: 27/05/2022
---
+++ python_scripts/matrix_operations.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+
+# ===================================
+# As quatro operações básicas
+# ===================================
+# Adição de matriz
+def soma_matriz(A, B):
+    pass
+
+
+# Subtração de matriz
+def subtrai_matriz(A, B):
+    pass
+
+
+# Produto de matriz
+def multiplica_matriz(A, B):
+    pass
+
+
+# Divisão de matriz
+def divide_matriz(A, B):
+
+    pass
+
+
+# ===================================
+# Operações básicas com escalar
+# ===================================
+# Produto por escalar
+def multiplica_escalar(A, num):
+    pass
+
+
+# ===================================
+# Operações complementares
+# ===================================
+# Cálculo de Determinante
+def calcula_determinante(A):
+    pass
+
+
+# Cálculo de inversa
+def calcula_inversa(A):
+    pass
+
+
+# Cálculo de Transposta
+def calcula_transposta(A):
+    pass
+
+
+# ====================================================
+#                    Área de teste
+# ====================================================
+
+M = np.array([["2", 2, 2], [2, 2, 2]])
+N = np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+O = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+
+P = np.array([1, 2, 3])
+Q = np.array([1, 2, 3])
+R = np.array([[1], [2], [3]])
+
+print()
+# soma_matriz(M, N)
+# subtrai_matriz(P, Q)
+# multiplica_matriz(P, R)
+# divide_matriz(N, O)
+# multiplica_escalar(A, num)
+# calcula_determinante(A)
+# calcula_inversa(A)
+# calcula_transposta(A)
+# np.linalg.inv(Q)
+# print(len(np.shape(O)))
