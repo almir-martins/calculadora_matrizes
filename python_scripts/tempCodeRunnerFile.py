a3b70527c184b13a86c62d68c6d25f0567491361
@crazyself <crazyself@gmail.com>
2022-06-07 19:13:49 -0300

    Criando a página para operações com duas matrizes

--- python_scripts/tempCodeRunnerFile.py
+++ python_scripts/tempCodeRunnerFile.py
@@ -1,31 +1,3 @@
 
-(
-    a1,
-    a2,
-) = st.columns([5, 5])
-x = a1.write("Matriz A")
-z = a2.write("Matriz B")
-
-c1, c2, c3, c4, c5, c6, c7, c8, c9, c10 = st.columns(10)
-a = c1.text_input("n1")
-b = c2.text_input("n2")
-c = c3.text_input("n3")
-d = c4.text_input("n4")
-e = c5.text_input("n5")
-f = c6.text_input("n6")
-g = c7.text_input("n7")
-t = c8.text_input("n8")
-r = c9.text_input("n9")
-s = c10.text_input("n10")
-
-d1, d2, d3, d4, d5, d6, d7, d8, d9, d10 = st.columns(10)
-h = d1.text_input("1")
-i = d2.text_input("2")
-j = d3.text_input("3")
-k = d4.text_input("4")
-l = d5.text_input("5")
-m = d6.text_input("6")
-n = d7.text_input("7")
-o = d8.text_input("8")
-p = d9.text_input("9")
-q = d10.text_input("10")
+# for k in list(numeros.keys()).sort():
+#     print(numeros[k])
